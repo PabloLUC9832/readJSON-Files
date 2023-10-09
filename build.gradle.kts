963f6de38e4b01daf325f2d87534e4103e1546b9
PabloLUC9832 <lucaspablo340@outlook.com>
2023-10-08 21:14:49 -0600

    lectura de multiples archvos pdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-// https://mvnrepository.com/artifact/com.googlecode.json-simple/json-simple
+    // https://mvnrepository.com/artifact/com.googlecode.json-simple/json-simple
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    // https://mvnrepository.com/artifact/org.apache.pdfbox/pdfbox
+    implementation("org.apache.pdfbox:pdfbox:2.0.1")
+
 
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.util.Charsets;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,7 +19,7 @@ public class Main {
         Reader reader = new FileReader("C:\\Users\\rdjr\\Documents\\Empleos\\conf.json");
         JSONObject jsonObject = (JSONObject) parser.parse(reader);
         System.out.println(jsonObject);
-        System.out.println("------");
+        System.out.println("------jsonObject--------");
 
         String jsonData = jsonObject.toString();
         String data = jsonData.replaceAll("\\[", "").replaceAll("\\]","")
@@ -26,20 +29,79 @@ public class Main {
         //https://www.studytonight.com/java-examples/how-to-convert-string-to-arraylist-in-java
         ArrayList<String> list = new ArrayList<>(Arrays.asList(data.split(",")));
         list.forEach(System.out::println);
-        System.out.println("--------");
+        System.out.println("--------KEY:VALUE-----");
 
-        String nPDF = list.get(0);
-        String nXML = list.get(1);
-        String nTXT = list.get(2);
+        //String nPDF = list.get(0);
+        String nPDF = listToString(list,0);
+        String nXML = listToString(list,1);
+        String nTXT = listToString(list,2);
 
-        String hostSFTP = list.get(5);
-        String portSFTP = list.get(4);
-        String userSFTP = list.get(6);
-        String passwordSFTP = list.get(3);
+        String passwordSFTP = listToString(list,3);
+        String portSFTP = listToString(list,4);
+        String hostSFTP = listToString(list,5);
+        String userSFTP = listToString(list,6);
+
+        System.out.println(nPDF);
+        System.out.println(nXML);
+        System.out.println(nTXT);
+        System.out.println(hostSFTP);
+        System.out.println(portSFTP);
+        System.out.println(userSFTP);
+        System.out.println(passwordSFTP);
+
+        //Lectura de archivos PDF, extrayendo la clave despues de las palabras Hola mundo
+
+        File dir = new File("C:\\Users\\rdjr\\Documents\\Empleos\\galvan\\input");
+        File[] files = dir.listFiles(new FilenameFilter()
+        {
+            // use anonymous inner class
+            @Override
+            public boolean accept(File dir, String name)
+            {
+                return name.toLowerCase().endsWith(".pdf");
+            }
+        });
+        System.out.println("--------TEXT FROM PDFs-------");
+        ArrayList<String> textsFromPDF = new ArrayList<>();
+        for (File file : files)
+        {
+            /*
+            int len = file.getAbsolutePath().length();
+            String txtFilename = file.getAbsolutePath().substring(0, len - 4) + ".txt";
+            // check whether txt file exists omitted
+
+            try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(txtFilename), Charsets.UTF_8);
+                 PDDocument document = PDDocument.load(file))
+            {
+                PDFTextStripper stripper = new PDFTextStripper();
+                stripper.writeText(document, out);
+            }
+            */
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper stripper = new PDFTextStripper();
+            //Retrieving text from PDF document
+            List<String> text = Arrays.asList(stripper.getText(document));
+            System.out.println(text);
+            textsFromPDF.add(String.valueOf(text));
+            //Closing the document
+            document.close();
+        }
+        System.out.println("------KEYS---------");
+        for (String s: textsFromPDF) {
+            //System.out.println(Arrays.toString(s.split(",")));
+            System.out.println(Arrays.toString(new String[]{s.replaceAll("\\[", "").replaceAll("\\]", "")}));
+        }
+        //System.out.println(textsFromPDF);
 
 
+    }
 
+    private static String listToString(List list,int index){
 
+        String data = (String) list.get(index);
+        String parts[] = data.split(":");
+        String value = parts[1].replaceAll("\"","");
+        return value;
     }
 
 }
